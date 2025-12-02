c452bc0774e44e6689f91d03be2a9eae9f75471c
Mohammad Rafiq <rafiq@rrv.sh>
2025-12-02 11:20:29 +0800

    part 2 done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,13 @@
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
 
-fn main() -> io::Result<()> {
-    // Day 1
-    let file = File::open("./inputs/01.txt")?;
-    let file = BufReader::new(file);
-
-    // Part 1
+fn day1part1() -> io::Result<()> {
     let mut dial = 50;
     let mut password = 0;
+    let file = File::open("./inputs/01.txt")?;
+    let file = BufReader::new(file);
     for line in file.lines() {
         let line = line?;
         let direction = &line[..1];
@@ -30,6 +28,52 @@ fn main() -> io::Result<()> {
         }
     }
     println!("Solution for Day 1 Part 1: {password}");
+    Ok(())
+}
 
+fn day1part2() -> io::Result<()> {
+    let mut dial = 50;
+    let mut password = 0;
+    for line in BufReader::new(File::open("./inputs/01.txt")?).lines() {
+        let line = line?;
+        let clicks = line[1..].parse::<i32>().expect("Parse integer error")
+            * if &line[..1] == "L" { -1 } else { 1 };
+        println!("dial: {dial}, clicks: {clicks}");
+        dial += clicks;
+        match dial.cmp(&0) {
+            Ordering::Less => {
+                if dial != clicks {
+                    password += 1;
+                }
+                if dial < -99 {
+                    password -= dial / 100;
+                    if dial % 100 == 0 {
+                        dial = 0;
+                    } else {
+                        dial = 100 + (dial % 100);
+                    }
+                } else {
+                    dial = 100 - dial.abs();
+                }
+            }
+            Ordering::Equal => {
+                password += 1;
+            }
+            Ordering::Greater => {
+                if dial > 99 {
+                    password += dial / 100;
+                    dial %= 100;
+                }
+            }
+        }
+        println!("password: {password}");
+    }
+    println!("Solution for Day 1 Part 2: {password}");
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    day1part1()?;
+    day1part2()?;
     Ok(())
 }
